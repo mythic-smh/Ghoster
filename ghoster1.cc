ed4bcc4c93ab1243e041eb3196e1fbad57032bae
mateo muldner <mateomuldner@gmail.com>
2023-03-12 22:15:03 -0400

    Wrote my first program

---
+++ ghoster1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to Ghosters" << endl;
+    cout << "To move you can use W for forward, A for left, D for right and finally S for backwards" << endl;
+    cout << "Please type your name" << endl;
+    cin >> NameS;
+    cout << "So you name is" << NameS << " ?" << endl;
+    cout << "If this is correct please click \"B\"
+    return 0;
+}
