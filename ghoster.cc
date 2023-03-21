5d1376153c573f31abdc56e4310ebd6d27dedecd
mateo muldner <mateomuldner@gmail.com>
2023-03-21 14:43:09 -0400

    added some if and else statements

--- ghoster.cc
+++ ghoster.cc
@@ -1,28 +1,39 @@
 #include <iostream>
-using namespace std;
 #include <string>
+using namespace std;
+
 int main() {
-    while (true) {
-    cout << "Welcome to ~Ghoster~" << endl;       
-    cout << "Please type your name" << endl;
+while (true) {
+    cout << "Welcome to ~Ghoster~"
+<< endl;
+    cout << "Please type your name."
+    << endl;
     string userName;
     cin >> userName;
-    cout << "So your name is " << userName << " ?" << endl;
-    cout << "If this is correct please type \"Yes\" " << endl;
+    cout << "So your name is " <<
+userName << " ?" << endl;
+    cout << "If this is correct please type \"Yes\""
+<< endl;
     string Answer;
     cin >> Answer;
-    if (Answer == "Yes"|| Answer == "yes") {
-    cout << "Hello " << userName << " I hope you enjoy!" << endl;
-} else {
+    if (Answer == "Yes" || Answer == 
+"yes") {
+    cout << "Hello " << userName <<
+" I hope you enjoy!" << endl;
+    } else {
     cout << "Syntax error please restart." << endl;
+    continue;
 }
-    cout << "To move you can use W for forward, A for left, D for right and finally S for backwards" << endl; 
+    cout << "Now we will get on how to move: W is for moving forward, A is for moving to the left, S if for moving backwards and   finally D is for moving to the right" <<
+endl;
     cout << "Please type \"Start\" to start the program." << endl;
     string Correct;
     cin >> Correct;
-    if (Correct == "Start" || Correct == "start") {
-    cout << "The game will now start:"
-} else {cout << "
+    if (Correct == "Start"|| Correct == "start") {
+        cout << "The game will now start:" << endl;
+    } else {
+    cout << "";
+    }
 }
 return 0;
 }
